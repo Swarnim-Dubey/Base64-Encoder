d8f32a3513325c3f84668cffa27fed66d8623c55
unknown <swarnimdubey135@gmail.com>
2025-11-08 16:06:29 +0530

    Made a baseline for the implementation

---
+++ base64-encoder.py
@@ -0,0 +1,18 @@
+# base64 encoding wihtout using any in-built library
+
+# defining all the characters that can be entered into the string
+alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
+print("WELCOME TO BASE64 ENCODER !!")
+print()
+
+def takeString():
+    a = input("Enter the string you want to convert : ")
+    print("Entered string to convert is : ",a)
+    return a
+
+def convertTobase64(a):
+    # logic later 
+    pass
+a = takeString()
+convertTobase64(a)
