aa40e59a2c9e91d5afb38a888e40ec5be9285068
unknown <swarnimdubey135@gmail.com>
2025-11-09 22:06:27 +0530

    added more

--- base64-encoder.py
+++ base64-encoder.py
@@ -18,9 +18,9 @@ def convertTobase64(a):
     b = " "
     for i in a:
         x = ord(i)              # get ASCII value
-        y = bin(x)[2:]          # convert to binary (remove 0b)
+        y = bin(x)[2 : ]          # convert to binary (remove 0b)
         while len(y) < 8:       # make sure binary is 8 bits
-            y = "0" + y
+            y += "0"
         b += y               # add to full binary string
 
     # add extra zeros if not multiple of 6
@@ -33,7 +33,7 @@ def convertTobase64(a):
     while i < len(b):
         s = b[i:i+6]            # take 6 bits
         num = int(s, 2)         # convert binary to number
-        ans = ans + alpha[num]  # pick that index from alpha string
+        ans += alpha[num]  # pick that index from alpha string
         i += 6
 
     # add '=' padding if needed
@@ -43,3 +43,17 @@ def convertTobase64(a):
         ans += "="
 
     print("Base64 Encoded string is : ", ans)
+
+def convertFrombase64(a):
+    pad = a.count("=")
+    a = a.replace("=", " ")
+    b = ""
+
+    for ch in a :
+        num = alpha.index(ch)
+        bits = bin(num)[2 : ]
+
+        while len(bits) < 6:
+            bits += "0"
+        b += bits
+    
